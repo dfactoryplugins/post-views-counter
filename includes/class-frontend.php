712ef74ba0b0c40b5051ddfc7ad3350c2f7b03f1
psielicki <48321567+psielicki@users.noreply.github.com>
2024-04-10 15:47:12 +0200

    Enqueue main script in header instead of footer

--- includes/class-frontend.php
+++ includes/class-frontend.php
@@ -247,7 +247,7 @@ class Post_Views_Counter_Frontend {
 
 		// specific counter mode?
 		if ( in_array( $mode, [ 'js', 'rest_api' ], true ) ) {
-			wp_enqueue_script( 'post-views-counter-frontend', POST_VIEWS_COUNTER_URL . '/js/frontend' . ( ! ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '.min' : '' ) . '.js', [], $pvc->defaults['version'], true );
+			wp_enqueue_script( 'post-views-counter-frontend', POST_VIEWS_COUNTER_URL . '/js/frontend' . ( ! ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '.min' : '' ) . '.js', [], $pvc->defaults['version'], false );
 
 			// prepare args
 			$args = [
