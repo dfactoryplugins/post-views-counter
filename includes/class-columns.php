a7699c0d2b0cb4f8661c34ab29fb520903140198
Bartosz Arendt <info@digitalfactory.pl>
2025-06-04 09:41:19 +0200

    Update class-columns.php

--- includes/class-columns.php
+++ includes/class-columns.php
@@ -214,6 +214,9 @@ class Post_Views_Counter_Columns {
 		if ( ! $pvc->options['display']['post_views_column'] || ! in_array( $post_type, $pvc->options['general']['post_types_count'] ) )
 			return $columns;
 		
+		if ( apply_filters( 'pvc_admin_display_post_views', true ) === false )
+			return $columns;
+		
 		// add new sortable column
 		$columns['post_views'] = 'post_views';
 
